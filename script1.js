e7d0e7be323702827f0287eda515ec9d6a38e370
Loromirov <117039676+Loromirov@users.noreply.github.com>
2024-03-21 22:43:22 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,22 +7,22 @@
 // // Свойство pages (количество страниц) - число, количество страниц в книге.
 // // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
-// class Book {
-//   constructor(title, author, pages) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//   }
-//   displayInfo() {
-//     console.log(
-//       `Это книга ${this.title}. Автор(ы) - ${this.author}. Количество страниц - ${this.pages}`
-//     );
-//   }
-// }
-// const book1 = new Book("Пикник на обочине", "братья Стругацкие", 231);
-// console.log(book1);
-// const book2 = new Book("Метро 2033", "Дмитрий Глуховский", 384);
-// console.log(book2);
+ class Book {
+   constructor(title, author, pages) {
+     this.title = title;
+     this.author = author;
+     this.pages = pages;
+   }
+   displayInfo() {
+     console.log(
+       `Это книга ${this.title}. Автор(ы) - ${this.author}. Количество страниц - ${this.pages}`
+     );
+   }
+ }
+ const book1 = new Book("Пикник на обочине", "братья Стругацкие", 231);
+ console.log(book1);
+ const book2 = new Book("Метро 2033", "Дмитрий Глуховский", 384);
+ console.log(book2);
 
-// book1.displayInfo();
-// book2.displayInfo();
+ book1.displayInfo();
+ book2.displayInfo();
